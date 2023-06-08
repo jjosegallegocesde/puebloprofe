a8f2456eb28b080fec997b5921f5c58bdd53eb97
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2023-06-08 17:15:04 -0500

    ok

--- app/src/main/java/com/example/probando/Home.java
+++ app/src/main/java/com/example/probando/Home.java
@@ -2,9 +2,16 @@ package com.example.probando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -15,12 +22,59 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        botonHoteles.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
+
+    }
+
+
+    //metodo para cambiar el idioma de mi app
+    public void cambiarIdioma(String idioma){
+
+        //configurando el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configracion global en el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
+    //Cargar el menu xml creado previamente
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+    //DAR funcionamiento al menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //observamos a que item del menu le damos clic
+        int itemSeleccionado=item.getItemId();
+
+        switch(itemSeleccionado) {
+
+            case(R.id.opcion1):
+                Toast.makeText(this, "seleccionaste opcion1", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.opcion2):
+                this.cambiarIdioma("en");
+                Intent intent1=new Intent(Home.this,Home.class);
+                startActivity(intent1);
+                break;
+            case(R.id.opcion3):
+                this.cambiarIdioma("es");
+                Intent intent2=new Intent(Home.this,Home.class);
+                startActivity(intent2);
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
 
     }
 }
